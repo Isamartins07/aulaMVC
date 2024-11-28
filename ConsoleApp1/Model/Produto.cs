8eb57cd59a83f4ebfd97f6ce2c9dc074b8eaa5cb
ISABELLE DA SILVA MARTINS <isabelle.smartins3@senacsp.edu.br>
2024-11-27 22:24:40 -0300

    Aula 27_11

---
+++ ConsoleApp1/Model/Produto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Model
+{
+
+    [Table("Produto")]
+    public partial class Produto
+    {
+        public int Id { get; set; }
+
+        [Column("NomeProduto")]
+        public string NomeProduto { get; set; }
+        [Required]
+
+        [Column("DescricaoProduto")]
+        public string DescricaoProduto { get; set; }
+        [Required]
+
+        [Column("ValorProduto")]
+        public decimal ValorProduto { get; set; }
+    }
+}
