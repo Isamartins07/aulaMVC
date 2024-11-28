6e0f731fa30f23f43a735523a1b6c5756ce433b9
ISABELLE DA SILVA MARTINS <isabelle.smartins3@senacsp.edu.br>
2024-11-28 20:08:41 -0300

    MVC

---
+++ ConsoleApp1/Controller/VendaController.cs
@@ -0,0 +1,24 @@
+﻿using ConsoleApp1.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//controller = pode ficar dentro do context
+//
+
+namespace ConsoleApp1.Controller
+{
+    internal class VendaController
+    {
+        public VendaController() : base(@"Server=JUN0675619W10-1\\BDSENAC; Database=BancoVendas; user id=senaclivre; password = senaclivre")
+        {
+            // Desabilita a criação automática de tabelas
+            Database.SetInitializer<VendaController>(null);
+        }
+    }
+
+    public DbSet<Vendas> venda { get; set; }
+}
---
+++ ConsoleApp1/Model/Venda.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+//model representa uma entidade = tabela,
+//model reprenta todas as informações para representar as tabelas 
+//informações que serão manipuladas
+
+namespace ConsoleApp1.Model
+{
+
+    [Table("Vendas")]
+    public partial class Venda
+    {
+        public int Id { get; set; }
+        [Column("Produto")]
+        public string Produto { get; set; }
+        [Required]
+
+
+        [Column("Descricao")]
+        public string Descricao{ get; set; }
+        [Required]
+
+        [Column("DataVenda")]
+        public DateTime DataVenda { get; set; }
+        [Required]
+
+        [Column("ValorProduto")]
+        public decimal ValorProduto { get; set; }
+
+
+    }
+}
